99b926acbe67cd2f8b9f06c53443feb88623b359
alfo0924 <146694784+alfo0924@users.noreply.github.com>
2024-07-15 11:24:31 +0800

    v1.01 commit n push

--- app/src/main/java/com/example/activitypractice2/MainActivity.java
+++ app/src/main/java/com/example/activitypractice2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.activitypractice2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,4 +23,28 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+    public void SendMobileName(View view)
+    {
+        //press the A button  that recieve  BMW s1000RR
+        String mobile;
+        if(view.getId()==R.id.buttonA)
+        {
+            mobile="BNW S1000R";
+        }
+        else {
+            mobile="AUDI R8";
+        }
+        Intent intent=new Intent(this,MobileActiviy.class);
+        Bundle bundle=new Bundle();
+        bundle.putString("mobile",mobile);
+        //use intent to bring data to another activity
+        intent.putExtras(bundle);
+        startActivity(intent);
+
+
+
+
+    }
+
+
 }
---
+++ app/src/main/java/com/example/activitypractice2/MobileActiviy.java
@@ -0,0 +1,46 @@
+package com.example.activitypractice2;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MobileActiviy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_mobile_activiy);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Bundle bundle=this.getIntent().getExtras();
+        if(bundle !=null)
+        {
+            String mobile =String.format(bundle.getString("mobile"));
+
+            TextView name=(TextView) findViewById(R.id.textView);
+            name.setText(mobile);
+
+            ImageView img=(ImageView)findViewById(R.id.imageView);
+            String filename=mobile.toLowerCase();
+            int imgId=getResources().getIdentifier(filename,"drawable",getPackageName());
+            Drawable drawableImg= ContextCompat.getDrawable(this,imgId);
+            img.setImageDrawable(drawableImg);
+
+        }
+
+
+    }
+    }
